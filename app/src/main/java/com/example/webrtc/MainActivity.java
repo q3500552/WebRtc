753fb1ccbba2d33180ca9c7d7018e1af7f2f78c5
jim <q3500552@gmail.com>
2020-05-11 16:03:34 +0800

    v1

---
+++ app/src/main/java/com/example/webrtc/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.webrtc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.main).setBackgroundColor(Color.rgb(0, 0, 0));
+    }
+}
